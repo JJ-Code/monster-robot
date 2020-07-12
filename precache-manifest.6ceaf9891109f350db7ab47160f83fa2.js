5ef843e997a1e96db18e926c46a6af9a6fbcb6ac
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-07-12 16:58:01 -0700

    Updates

---
+++ precache-manifest.6ceaf9891109f350db7ab47160f83fa2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c88de6b2a2db0e4cdd02c179ed76fe",
+    "url": "/monster-robot/index.html"
+  },
+  {
+    "revision": "d048e7e8ccde89fecb6f",
+    "url": "/monster-robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "79e61f838366de7ac275",
+    "url": "/monster-robot/static/css/main.29bf7947.chunk.css"
+  },
+  {
+    "revision": "d048e7e8ccde89fecb6f",
+    "url": "/monster-robot/static/js/2.9a8a60be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-robot/static/js/2.9a8a60be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e61f838366de7ac275",
+    "url": "/monster-robot/static/js/main.ac0ee610.chunk.js"
+  },
+  {
+    "revision": "d825840f65bf842b19e0",
+    "url": "/monster-robot/static/js/runtime-main.28ed9128.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/monster-robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
