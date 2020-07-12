61729f2b35ead2cae7f1ffbcd98a6c38bb534166
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-07-11 20:50:55 -0700

    added useEffect to run a fetch data api and added scroll components

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import CardList from "./components/card-list/CardList";
 import SearchBox from "./components/search-box/SearchBox";
-import { robots } from './robots';
+import Scroll from "./components/scroll/Scroll";
+//import { robots } from './robots';
 
 import './App.css';
 
 
 const App = () => {
   const initalState = {
-    robotFriends: robots,
+    robotFriends: [],
     searchField: "",
   }
-
   const [robotSearch, setRobotSearch] = useState(initalState)
-
   const { robotFriends, searchField } = robotSearch;
 
-  //console.log(robotSearch.robotFriends);
+  const fetchRobots = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(robots => setRobotSearch({ robotFriends: robots, searchField: "" }))
+  }
+
+  useEffect(() => {
+    fetchRobots()
+    console.log("I ran");
+
+  }, [])
+
 
   const onSearchChange = (e) => {
     e.preventDefault();
     if (e.target.value !== '') {
-      console.log(e.target.value);
+      //console.log(e.target.value);
       let results = robotFriends.filter(robotFriend => robotFriend.name.toLowerCase().includes(searchField.toLowerCase()))
       setRobotSearch({ ...robotSearch, robotFriends: results, searchField: e.target.value }
       )
     } else {
-      setRobotSearch(initalState);
+      fetchRobots();
     }
   }
 
+  console.log(robotFriends);
 
   return (
+ 
+    
     <div className='App tc'>
       <h1 className='f1'>Robot Friends</h1>
-      <SearchBox onSearchChange={onSearchChange} />
-      <CardList robots={robotFriends} />
+
+      {robotFriends.length === 0 ? (<h1>Loading...</h1>) :
+        (<Fragment>
+          <SearchBox onSearchChange={onSearchChange} />
+          <Scroll>
+            <CardList robots={robotFriends} />
+            </Scroll>
+        </Fragment>)}
     </div>
   );
 };
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 
 const Card = ({ id, name, username, email }) => {
-  console.log("I there");
-
 
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5'>
