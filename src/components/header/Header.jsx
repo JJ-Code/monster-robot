d45eeebfdacdaec7c03465ba9dd107fc27a9df5e
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-07-19 17:05:57 -0700

    added header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SearchBox from "../search-box/SearchBox";
+import { connect } from 'react-redux';
+import { setCurrentSearch } from "../../actions/robotsActions";
+
+const Header = ({ setCurrentSearch }) => {
+  console.log("header");
+  const onSearchChange = (e) => {
+    e.preventDefault();
+    setCurrentSearch(e.target.value)
+  }
+  return (
+    <div>
+      <h1 className='f1'>Robot Friends</h1>
+      <SearchBox onSearchChange={onSearchChange} />
+    </div>
+  )
+}
+
+export default connect(null, { setCurrentSearch })(Header);
